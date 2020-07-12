ac68f3382bcb97c0fb5a6c93db939da9b9ca55c4
sheunaluko <alukosheun@gmail.com>
2020-07-12 05:58:50 +0000

    fix folder

--- src/tsd.ts
+++ src/tsd.ts
@@ -1,6 +1,6 @@
 //import the tidyscripts deno modules 
 
-import * as tsd from "../tidyscripts/src/deno/index.ts" //may need edit 
+import * as tsd from "../../tidyscripts/src/deno/index.ts" //may need edit 
 
 export {tsd} 
 
